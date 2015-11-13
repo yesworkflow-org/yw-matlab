4be7f4baa69e4a1e28aee173f7b7b8e5e8b1f6b1
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-11-12 19:42:13 -0800

    Changed script to function that takes optional argument.

--- src/main/resources/scripts/init_run_manager.m
+++ src/main/resources/scripts/init_run_manager.m
@@ -1,12 +1,18 @@
-import org.dataone.client.configure.Configuration;
-import org.dataone.client.run.RunManager;
+% Creates a run manager corresponding to the provided configuration
+% directory or the current directory if no argument is provided.
+function [run_mgr] = init_run_manager(run_manager_config_dir)
 
-% prompt for the configuration directory
-run_manager_config_dir = input('Configuration directory: ', 's');
+    % use the current directory as the config directory if no argument passed
+    if nargin == 0
+        run_manager_config_dir = pwd;
+    end
 
-% create a run manager associated with the given configuration directory
-run_manager_config = Configuration('configuration_directory', run_manager_config_dir);
-run_mgr = RunManager.getInstance(run_manager_config);
-clear run_manager_config_dir run_manager_config
+    % create a configuration corresponding to the given directory
+    import org.dataone.client.configure.Configuration;
+    run_manager_config = Configuration('configuration_directory', run_manager_config_dir);
+    
+    % create a run manager associated with the configuration
+    import org.dataone.client.run.RunManager;
+    run_mgr = RunManager.getInstance(run_manager_config);
 
-
+end
