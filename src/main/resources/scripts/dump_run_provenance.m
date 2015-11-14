de1f802f51d4004a438ef48a40da137574f0fc35
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-11-13 18:50:44 -0800

    Dump of run provenance is now YAML.

--- src/main/resources/scripts/dump_run_provenance.m
+++ src/main/resources/scripts/dump_run_provenance.m
@@ -12,18 +12,22 @@ function dump_run_provenance(run_dir)
     executionWorkspaceFile = [run_parent_directory '/' run_id '/' run_id '.mat'];
     load(executionWorkspaceFile);
 
-    disp('Inputs:')
+    disp(' ')
+    disp('inputs:')
     for input_id=executionObj.execution_input_ids
         input_object = executionObj.execution_objects(input_id{1});
         input_file_path = input_object.get('full_file_path');
-        disp(input_file_path)
+        disp(['  - ' input_file_path]);
     end
-
-    disp('Outputs:')
+    
+    disp(' ')
+    disp('outputs:')
     for output_id=executionObj.execution_output_ids
         output_object = executionObj.execution_objects(output_id{1});
         output_file_path = output_object.get('full_file_path');
-        disp(output_file_path)
+        disp(['  - ' output_file_path]);
     end
+    
+    
 
 end
