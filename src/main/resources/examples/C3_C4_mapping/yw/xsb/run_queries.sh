a81213388bbd82139282a8382b672c5e00b68ca7
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-11-18 15:23:47 -0800

    Fixed typos and simplified descriptions in query definition files.

--- src/main/resources/examples/C3_C4_mapping/yw/xsb/run_queries.sh
+++ src/main/resources/examples/C3_C4_mapping/yw/xsb/run_queries.sh
@@ -16,13 +16,13 @@ printall('eq1(SourceFile) - What source files were YW annotations extracted from
 printall('eq2(ProgramName) - What are the names of all program blocks in the script?', eq2(_)).
 printall('eq3(PortName) - What out ports are qualified with URIs?', eq3(_)).
 
-printall('mq1(SourceFile,StartLine,EndLine) - Where is the definition of block C3_C4_map_present_NA.fetch_monthly_mean_precipitation_data?', mq1(_,_,_)).
+printall('mq1(SourceFile,StartLine,EndLine) - Where is the definition of program block fetch_monthly_mean_precipitation_data?', mq1(_,_,_)).
 printall('mq2(WorkflowName) - What is the name of the top-level workflow?', mq2(_)).
 printall('mq3(ProgramName) -  What are the names of the program blocks comprising the workflow?', mq3(_)).
 printall('mq4(ProgramName) - What are the names of the program blocks in the workflow that produce workflow outputs?', mq4(_)).
 printall('mq5(DataName) - What are the inputs to the script?', mq5(_)).
-printall('mq6(DataName) - What data is output by program block C3_C4_map_present_NA.examine_pixels_for_grass?', mq6(_)).
-printall('mq7(ProgramName) - What program blocks provide input directly to C3_C4_map_present_NA.generate_netcdf_file_for_Grass_fraction?', mq7(_)).
+printall('mq6(DataName) - What data is output by program block examine_pixels_for_grass?', mq6(_)).
+printall('mq7(ProgramName) - What program blocks provide input directly to generate_netcdf_file_for_Grass_fraction?', mq7(_)).
 printall('mq8(ProgramName) - What program blocks have input ports that receive data lon_variable?', mq8(_)).
 printall('mq9(PortCount) - How many ports read data lat_variable?', mq9(_)).
 printall('mq10(DataCount) - How many data are read by more than port in workflow C3_C4_map_present_NA?', mq10(_)).
@@ -38,7 +38,7 @@ printall('mq19(UriVariableName) - What URI variables are associated with reads o
 printall('mq20(UpStreamDataName) - What URI variables do data read into mean_precip and mean_airtemp have in common?', mq20(_)).
 
 printall('rq1(InputFile) - What input files were used to compose the rain matrix?', rq1(_)).
-printall('rq2(InputCount) - How many input files were used to compose the air tempurature matrix?', rq2(_)).
+printall('rq2(InputCount) - How many input files were used to compose the air temperature matrix?', rq2(_)).
 printall('rq3(InputFile) - What input files provided the data used to derive the workflow output Grass_fraction_data?', rq3(_)).
 printall('rq4(StartYear, EndYear) - What is the range of years over which the data in the mean_precip input files were collected?', rq4(_,_)).
 printall('rq5(Month) - What months of the year do the mean_airtemp input files correspond to?', rq5(_)).
