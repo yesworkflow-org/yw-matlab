bf638c17a67026987b0b47cad95109114f5b6c85
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-12-02 11:30:32 -0800

    export_run script now takes configuration from yw.properties file.

--- src/main/resources/scripts/export_run.m
+++ src/main/resources/scripts/export_run.m
@@ -1,53 +1,55 @@
 
 
-function export_run(run_prov_dir, run_base_dir)
+function export_run()
 
-    % use the current directory as the run provenance directory if no
-    % argument provided
-    if nargin < 1 || strcmp(run_prov_dir, '.')
-        run_prov_dir = pwd;
-    end
+    % load the yw.properties file
+    prop_file_path = [pwd '/yw.properties'];
+    fileReader = java.io.FileReader(java.lang.String(prop_file_path));
+    properties = java.util.Properties();
+    properties.load(fileReader);
+    fileReader.close();
 
-    % use the current directory as the run base directory if second
-    % argument not provided
-    if nargin < 2 || strcmp(run_base_dir, '.')
-        run_base_dir = pwd;
-    end
+    % use properties or default values to configure export
+    run_base_dir = get_property_or_default(properties, 'recon.rundir', '.');
+    provdir = get_property_or_default(properties, 'matlab.provdir', '.');
+    runid = get_property_or_default(properties, 'matlab.runid', '1');
+    export_file = get_property_or_default(properties, 'matlab.exportfile', 'run.yaml');
 
-    % load the execution object
-    [run_parent_directory,run_id] = fileparts(run_prov_dir);
-    executionWorkspaceFile = [run_parent_directory '/' run_id '/' run_id '.mat'];
+    % load the execution workspace for the run
+    executionWorkspaceFile = [provdir '/runs/' runid '/' runid '.mat'];
     load(executionWorkspaceFile);
-
-    % write base-relative file paths of input and output files for run
-    fileID = fopen('run.yaml','w');
-    write_file_paths(fileID, 'inputs', run_base_dir, executionObj, executionObj.execution_input_ids);
-    write_file_paths(fileID, 'outputs', run_base_dir, executionObj, executionObj.execution_output_ids);
-    fclose(fileID);
     
+    % write base-relative file paths of input and output files for run
+    fileID = fopen(export_file,'w');
+    run_base_path = char(java.io.File(run_base_dir).getCanonicalPath());
+    write_file_paths(fileID, 'inputs', run_base_path, executionObj, executionObj.execution_input_ids);
+    write_file_paths(fileID, 'outputs', run_base_path, executionObj, executionObj.execution_output_ids);
+    fclose(fileID);
+end
+
+function [value] = get_property_or_default(properties, propertyName, defaultValue)
+    if (properties.containsKey(propertyName))
+        value = properties.get(propertyName);
+    else 
+        value = defaultValue;
+    end
 end
 
 function write_file_paths(fileID, section_name, base_dir, executionObj, file_object_ids)
-
     fprintf(fileID, ['\n' section_name ':' '\n']);
-    
     for object_id=file_object_ids
         execution_object = executionObj.execution_objects(object_id{1});
         full_file_path = execution_object.get('full_file_path');
         base_relative_path = get_base_relative_path(base_dir, full_file_path);
         fprintf(fileID, ['  - ' base_relative_path '\n']);
     end
-
 end
     
 function [base_relative_path] = get_base_relative_path(base_dir, full_path)
-
     [one,base_dir_length] = size(base_dir);
-    
     if strncmpi(full_path, base_dir, base_dir_length)
-        base_relative_path = full_path(base_dir_length+1:end);
+        base_relative_path = full_path(base_dir_length+2:end);
     else
         base_relative_path = full_path;
     end
-    
 end
