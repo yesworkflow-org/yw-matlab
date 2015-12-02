e107c9d5575c09e64d6627f581c4ccbc6630d296
Timothy McPhillips <tmcphillips@absoluteflow.org>
2015-12-01 17:21:56 -0800

    Factored export_run script into functions.

--- src/main/resources/scripts/export_run.m
+++ src/main/resources/scripts/export_run.m
@@ -1,52 +1,53 @@
 
 
-function export_run(run_prov_dir, script_exec_dir)
+function export_run(run_prov_dir, run_base_dir)
 
-    EOL = '\n';
-
-    % use the current directory as the run provenance directory if no argument passed
+    % use the current directory as the run provenance directory if no
+    % argument provided
     if nargin < 1 || strcmp(run_prov_dir, '.')
         run_prov_dir = pwd;
     end
 
-    if nargin < 2 || strcmp(script_exec_dir, '.')
-        script_exec_dir = pwd;
+    % use the current directory as the run base directory if second
+    % argument not provided
+    if nargin < 2 || strcmp(run_base_dir, '.')
+        run_base_dir = pwd;
     end
-    [one,script_exec_dir_length] = size(script_exec_dir);
-
-    disp(script_exec_dir_length)
 
     % load the execution object
     [run_parent_directory,run_id] = fileparts(run_prov_dir);
     executionWorkspaceFile = [run_parent_directory '/' run_id '/' run_id '.mat'];
     load(executionWorkspaceFile);
 
+    % write base-relative file paths of input and output files for run
     fileID = fopen('run.yaml','w');
+    write_file_paths(fileID, 'inputs', run_base_dir, executionObj, executionObj.execution_input_ids);
+    write_file_paths(fileID, 'outputs', run_base_dir, executionObj, executionObj.execution_output_ids);
+    fclose(fileID);
     
-    fprintf(fileID, [EOL 'inputs:' EOL]);
-    for input_id=executionObj.execution_input_ids
-        input_object = executionObj.execution_objects(input_id{1});
-        input_file_path = input_object.get('full_file_path');
-        if strncmpi(input_file_path, script_exec_dir, script_exec_dir_length)
-            r = script_exec_dir_length;
-        else
-            r = 1;
-        end
-        fprintf(fileID, ['  - ' input_file_path(r+1:end) EOL]);
-    end
+end
+
+function write_file_paths(fileID, section_name, base_dir, executionObj, file_object_ids)
+
+    fprintf(fileID, ['\n' section_name ':' '\n']);
     
-    fprintf(fileID, [EOL 'outputs:' EOL]);
-    for output_id=executionObj.execution_output_ids
-        output_object = executionObj.execution_objects(output_id{1});
-        output_file_path = output_object.get('full_file_path');
-        if strncmpi(output_file_path, script_exec_dir, script_exec_dir_length)
-            r = script_exec_dir_length;
-        else
-            r = 1;
-        end
-        fprintf(fileID, ['  - ' output_file_path(r+1:end) EOL]);
+    for object_id=file_object_ids
+        execution_object = executionObj.execution_objects(object_id{1});
+        full_file_path = execution_object.get('full_file_path');
+        base_relative_path = get_base_relative_path(base_dir, full_file_path);
+        fprintf(fileID, ['  - ' base_relative_path '\n']);
     end
 
-    fclose(fileID);
+end
+    
+function [base_relative_path] = get_base_relative_path(base_dir, full_path)
+
+    [one,base_dir_length] = size(base_dir);
+    
+    if strncmpi(full_path, base_dir, base_dir_length)
+        base_relative_path = full_path(base_dir_length+1:end);
+    else
+        base_relative_path = full_path;
+    end
     
 end
